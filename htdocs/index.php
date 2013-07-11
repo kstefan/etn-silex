c92d22994c8db5778e522f3944009d7ce05b9ef2
Karel Stefan <karel.stefan@etnetera.cz>
2013-07-11 20:53:00 +0200

    core and hello world

---
+++ htdocs/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = require __DIR__.'/../src/app.php';
+
+require __DIR__.'/../config/app.php';
+require __DIR__.'/../src/controllers.php';
+
+$app->run();
